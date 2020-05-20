35ac9daf39c305eba8599591e44cd12f0e9d31b3
Dante L'Erario <dantelerario@gmail.com>
2020-05-20 17:09:16 +0200

    database updated with images, some css fixes

--- data/data.php
+++ data/data.php
@@ -1,51 +1,61 @@
 <?php
 $albums = [
   [
+    'img' => 'https://i.pinimg.com/474x/36/1f/f0/361ff052f89dd2b7c4aed9518d0fb3bb.jpg',
     'artist' => 'Metallica',
     'title' => 'Master Of Puppets',
     'genre' => 'Heavy Metal',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/41kj36cVMFL._AC_SX466_.jpg',
     'artist' => 'AC/DC',
     'title' => 'Back in Black',
     'genre' => 'Heavy Metal',
   ],
   [
+    'img' => 'https://i.ebayimg.com/images/g/HQ0AAOxy79JRgJ-V/s-l300.jpg',
     'artist' => 'System of a Down',
     'title' => 'Mesmerize',
     'genre' => 'Heavy Metal',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/71vSn%2BlK4WL._AC_SL1425_.jpg',
     'artist' => 'Back Sabbath',
     'title' => 'Paranoid',
     'genre' => 'Heavy Metal',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/71Okj3gDOxL._AC_SX466_.jpg',
     'artist' => 'Pink Floyd',
     'title' => 'The Wall',
     'genre' => 'Rock',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/61mGI-Zhr6L._AC_SX425_.jpg',
     'artist' => 'U2',
     'title' => 'The Joshua Tree',
     'genre' => 'Rock',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/81IIHrrH7PL._AC_SL1500_.jpg',
     'artist' => 'Pearl Jam',
     'title' => 'Ten',
     'genre' => 'Rock',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/613DVHvBFmL._AC_SX466_.jpg',
     'artist' => 'Dire Straits',
     'title' => 'Brothers in Arms',
     'genre' => 'Rock',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/41UwzAzQiIL._AC_SX466_.jpg',
     'artist' => 'Eminem',
     'title' => 'The Eminem Show',
     'genre' => 'Rap',
   ],
   [
+    'img' => 'https://images-na.ssl-images-amazon.com/images/I/71varhlbmYL._AC_SL1200_.jpg',
     'artist' => 'Drake',
     'title' => 'Scorpion',
     'genre' => 'Rap',
--- partial/main.php
+++ partial/main.php
@@ -4,7 +4,7 @@
     include_once __DIR__ . '/../data/data.php';
     foreach ($albums as $album) { ?>
           <div class="box">
-            <img src="https://i.pinimg.com/474x/36/1f/f0/361ff052f89dd2b7c4aed9518d0fb3bb.jpg" alt="img">
+            <img src="<?php echo $album['img']; ?>" alt="img">
             <p class="title"><?php echo $album['title']; ?></p>
             <p class="artist"><?php echo $album['artist']; ?></p>
             <p class="genre"><?php echo $album['genre']; ?></p>
